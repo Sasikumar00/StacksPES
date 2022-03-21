d69744bbc67b9e19720ffe6912f5ab574dd6ee8a
WINDY-WINDWARD <karthik.sarode23@gmail.com>
2022-03-22 00:50:54 +0530

    need to fix font size in pre to post but JS is complete

---
+++ assets/pre_post.js
@@ -0,0 +1,78 @@
+
+// function to check if character
+// is operator or not
+function isOperator(x) {
+    switch (x) {
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+            return true;
+    }
+    return false;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Convert prefix to Postfix expression
+async function preToPost(pre_exp) {
+
+    let s = [];
+
+    // length of expression
+    let length = pre_exp.length;
+
+    // reading from right to left
+    for (let i = length - 1; i >= 0; i--) {
+
+        // check if symbol is operator
+        if (isOperator(pre_exp[i])) {
+            // pop two operands from stack
+            let op1 = s[s.length - 1];
+            s.pop();
+            popit();
+            await sleep(3000);
+            let op2 = s[s.length - 1];
+            s.pop();
+            popit();
+            await sleep(3000);
+
+            // concat the operands and operator
+            let temp = op1 + op2 + pre_exp[i];
+
+            // Push String temp back to stack
+            s.push(temp);
+            pushit(temp);
+            document.getElementById("pre").value =  (s.join(""));
+            await sleep(3000);
+
+        }
+
+        // if symbol is an operand
+        else {
+            // push the operand to the stack
+            s.push(pre_exp[i] + "");
+            pushit(pre_exp[i]);
+            document.getElementById("pre").value = (s.join(""));
+            await sleep(3000);
+
+        }
+    }
+
+    // stack contains only the Postfix expression
+    
+    popit();
+    document.getElementById("pre").value = (s[s.length - 1]);
+    await sleep(3000);
+}
+
+function start_conversion() {
+    let data = document.getElementById("num").value;
+    // infixToPrefix(data);
+    preToPost(data);
+}
+
+// let pre_exp = "*-A/BC-/AKL";
+// console.log(preToPost(pre_exp));
